833c3dfa0faf39169835a8961ef7d8ddbce706e5
Romeu Castro <romeu.castro@iteriam.es>
2022-10-10 13:16:29 +0200

    City tests passed

--- src/main/java/com/devsuperior/bds02/resources/CityResource.java
+++ src/main/java/com/devsuperior/bds02/resources/CityResource.java
@@ -1,9 +1,56 @@
 package com.devsuperior.bds02.resources;
 
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cities")
 public class CityResource {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO cityDTO) {
+        CityDTO newCityDTO = service.insert(cityDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newCityDTO.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(newCityDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,7 +1,34 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+    @Transactional
+    public List<CityDTO> findAll() {
+        return repository.findAll().stream()
+                .sorted(Comparator.comparing(City::getName))
+                .map(CityDTO::new).collect(Collectors.toList());
+    }
+
+    public CityDTO insert(CityDTO cityDTO) {
+        City entity = new City(cityDTO.getId(), cityDTO.getName());
+        return new CityDTO(repository.save(entity));
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
